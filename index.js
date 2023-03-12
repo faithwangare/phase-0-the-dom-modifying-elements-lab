e512e24cbde3f730e050ac4e0a84150549146ea2
Georgina <faithwangare750@gmail.com>
2023-03-13 00:17:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here!/
+const element = document.getElementById("main");
+element.remove("main");
+const newHeader = document.createElement("h1");
+//creating h1 id
+newHeader.id= "victory"
+//creating innerHtml inside h1
+newHeader.innerHTML = "Gina is the champion"
